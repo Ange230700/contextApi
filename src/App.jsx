f394d09c607ce3dec7ef2a98e50dff9a557a2afd
Ange230700 <kouakouangeericstephane@gmail.com>
2023-11-06 10:49:39 +0100

    Doing challenge - creating a state

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import "./App.css";
 import UserProfile from "./components/UserProfile";
 
 function App() {
+
+  const [isOnline, setIsOnline] = useState(false);
+
   return <UserProfile />;
 }
 
