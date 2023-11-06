c7c4a73deb1527d04dcb6b17130edf07a96cb833
Ange230700 <kouakouangeericstephane@gmail.com>
2023-11-06 11:54:15 +0100

    Doing challenge - changing the text so that it shows offline or online according to the use case

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,10 +1,19 @@
-function UserProfile() {
+import PropTypes from "prop-types";
+
+function UserProfile({ isOnline, setIsOnline }) {
   return (
     <>
-      <div>User is</div>
-      <button>click to change user status</button>
+      <div>
+        User Profile is
+        <span> {isOnline ? "Online" : "Offline"}</span>
+      </div>
     </>
   );
 }
+
+UserProfile.propTypes = {
+  isOnline: PropTypes.bool.isRequired,
+  setIsOnline: PropTypes.func.isRequired,
+};
 
 export default UserProfile;
